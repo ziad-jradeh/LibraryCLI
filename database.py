b4539e9a1517acacb66e1bb8a4be3a724fbf8a73
elen-ladder <elen.ladder@gmail.com>
2023-03-31 13:50:27 +0200

    add search by name

--- database.py
+++ database.py
@@ -113,5 +113,27 @@ def create_database():
         connection.close()
         return "Database created successfully."
     
+def search_by_name_func(name):
+    
+                 
+        cur.execute(f"""SELECT Cast(book_id As Varchar), b.book_title, CAST(total_pages as Varchar) , g.genre_name, a.author_name,
+    CASE 
+    WHEN (b.number_copy > ((SELECT COUNT(*) FROM borrowing WHERE book_id=b.book_id) 
+                            - (SELECT COUNT(*) FROM returnings WHERE book_id=b.book_id))) 
+		  THEN 'True'
+
+    ELSE 'False'
+
+    END AS Availability
+    FROM books b
+    LEFT JOIN genres AS g ON b.genre_id = g.genre_id
+    LEFT JOIN authors AS a ON b.author_id = a.author_id
+    WHERE b.book_title ILIKE '%{name}%'
+        """)
+        return cur.fetchall()
+        
+  
+    
+    
   
         
